6afa56c489c7515b9f23af5f10173e32aedc94b2
Mahen Perera <kmaperera864@gmail.com>
2025-12-04 23:50:00 +0530

    Create Student DAO.

---
+++ src/main/java/com/example/springcruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.example.springcruddemo.dao;
+
+import com.example.springcruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}
---
+++ src/main/java/com/example/springcruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,25 @@
+package com.example.springcruddemo.dao;
+
+import com.example.springcruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+
+    private EntityManager entityManager;
+
+    // Inject Entity Manager using Constructor Injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
