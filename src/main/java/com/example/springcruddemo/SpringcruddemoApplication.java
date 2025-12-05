da0decb8fec8564e62ad6be4087575bf84f05c9b
Mahen Perera <kmaperera864@gmail.com>
2025-12-05 22:38:09 +0530

    Add method to save multiple objects.

--- src/main/java/com/example/springcruddemo/SpringcruddemoApplication.java
+++ src/main/java/com/example/springcruddemo/SpringcruddemoApplication.java
@@ -17,8 +17,24 @@ public class SpringcruddemoApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) { // Inject StudentDAO
         return runner -> {
-            createStudent(studentDAO);
+//            createStudent(studentDAO);
+            
+            createMultipleStudents(studentDAO);
         };
+        
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+
+        System.out.println("Creating 3 student objects ...");
+        Student tempStudent1 = new Student("Nicky", "Romero", "nicky@test.com");
+        Student tempStudent2 = new Student("Steve", "Angello", "steve@test.com");
+        Student tempStudent3 = new Student("Martin", "Garrix", "martin@test.com");
+
+        System.out.println("Saving the students ...");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
     }
 
     private void createStudent(StudentDAO studentDAO) {
