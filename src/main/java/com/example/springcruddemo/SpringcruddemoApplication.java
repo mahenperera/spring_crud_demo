037701d50c47d490b6251716e5490564aea0a4ed
Mahen Perera <kmaperera864@gmail.com>
2025-12-06 16:04:12 +0530

    Add DAO method to update a student.

--- src/main/java/com/example/springcruddemo/SpringcruddemoApplication.java
+++ src/main/java/com/example/springcruddemo/SpringcruddemoApplication.java
@@ -23,10 +23,25 @@ public class SpringcruddemoApplication {
 //            createMultipleStudents(studentDAO);
 //            readStudent(studentDAO);
 //            queryForStudents(studentDAO);
+//            queryForStudentsByLastName(studentDAO);
 
-            queryForStudentsByLastName(studentDAO);
+            updateStudent(studentDAO);
         };
         
+    }
+
+    private void updateStudent(StudentDAO studentDAO) {
+
+        int studentId = 1;
+        System.out.println("Getting student with id: " + studentId);
+        Student myStudent = studentDAO.findById(studentId);
+
+        System.out.println("Updating student ...");
+        myStudent.setFirstName("Abisheka");
+
+        studentDAO.update(myStudent);
+
+        System.out.println("Updated student: " + myStudent);
     }
 
     private void queryForStudentsByLastName(StudentDAO studentDAO) {
